f7bbaaf59506fff316444516e7e9678cfbbe16a2
KatalinDorman <dkata75@gmail.com>
2024-09-28 14:15:17 +0200

    car rental project created

---
+++ src/main/java/homework/sb_carrental_mvc/SbCarRentalMvcApplication.java
@@ -0,0 +1,13 @@
+package homework.sb_carrental_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbCarRentalMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbCarRentalMvcApplication.class, args);
+	}
+
+}
